cf9bdd5c4601c8e864bd36e3c3effb497593a5bf
ittuann <ittuann@outlook.com>
2026-06-11 17:48:28 +0800

    feat: update icon and avatar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="air wish" />
+      </head>
       <body
         className={`${fontMono.variable} ${fontSans.variable} font-mono antialiased`}
       >
